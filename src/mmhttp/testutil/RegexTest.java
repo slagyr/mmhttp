547264d1a70e898ab41c0dbe93d2fcaa326ce17f
Micah <micahmartin@gmail.com>
2020-11-16 12:12:52 -0700

    fix file encodings

--- src/mmhttp/testutil/RegexTest.java
+++ src/mmhttp/testutil/RegexTest.java
@@ -1,4 +1,4 @@
-//- Copyright ©2009 Micah Martin.  All Rights Reserved
+//- Copyright Â©2009 Micah Martin.  All Rights Reserved
 //- MMHTTP and all included source files are distributed under terms of the GNU LGPL.
 
 package mmhttp.testutil;
